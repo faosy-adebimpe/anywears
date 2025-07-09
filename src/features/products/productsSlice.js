ee091877c4da8a820db509b1fc72cdbee029e096
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-07-09 09:03:07 -0700

    Update productsSlice.js

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,6 +1,6 @@
 import { buildCreateSlice, asyncThunkCreator } from '@reduxjs/toolkit';
 
-const url = 'https://fakestoreapi.in/products';
+const url = 'https://fakestoreapi.in/api/products';
 // const url = 'http://127.0.0.1:5000/products';
 
 export const createAppSlice = buildCreateSlice({
