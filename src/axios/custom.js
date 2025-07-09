4dd40ad3ce31dd1567951b8e78561666497f24e3
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-07-09 08:34:06 -0700

    change https://fakestoreapi.com to https://fakestoreapi.in

--- src/axios/custom.js
+++ src/axios/custom.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const authFetch = axios.create({
-    baseURL: 'https://fakestoreapi.com',
+    // baseURL: 'https://fakestoreapi.com',
+    baseURL: 'https://fakestoreapi.in',
 });
 
 export default authFetch;
